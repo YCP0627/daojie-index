6500bd160b10b9e24bf239546416eff953c47c0f
YCP0627 <1261675173@qq.com>
2019-12-20 16:53:33 +0800

    忽略

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header">
+        <div class="city">
+            
+        </div>
+        <div class="user-operate">
+            <a href="javascript:;">我的订单</a>
+            <a href="javascript:;">登录</a>
+        </div>
+    </div>
+</template>
+
+export default {
+
+}
